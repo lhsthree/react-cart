67174f26e8e3a37246267ead8479ea1b1c396717
lhsthree <lhsthree@gmail.com>
2021-02-23 11:51:50 -0600

    feat: add clear cut button using reducer

--- src/context.js
+++ src/context.js
@@ -8,17 +8,22 @@ const AppContext = React.createContext()
 const initialState = {
   loading:false,
   cart: cartItems,
-  total:0,
+  total:5,
   amount:0,
 }
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer,initialState)
 
+const clearCart = () => {
+  dispatch({type:'CLEAR_CART'})
+}
+
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
       }}
     >
       {children}
